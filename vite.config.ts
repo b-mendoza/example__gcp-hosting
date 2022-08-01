c8271d32d809412712a7ffe1d4dc7c0168f3140e
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-08-01 01:19:06 -0600

    build: update vite conifg

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,10 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line import/no-unused-modules
-export default defineConfig(({ mode }) => {
-  const base = mode === 'production' ? '/example-paybotic-storage/' : '/';
-
-  return {
-    base,
-    build: {
-      sourcemap: true,
-    },
-    envDir: './env',
-    plugins: [react(), tsconfigPaths(), svgrPlugin()],
-  };
+export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
+  envDir: './env',
+  plugins: [react(), tsconfigPaths(), svgrPlugin()],
 });
